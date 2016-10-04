c2cf008ce1a78c3bbe8ed67ecf7eb5831aaaace3
Athithyaa <athithyapanchapakesan@gmail.com>
2016-10-03 21:07:16 -0600

    Adding basic database functionality         * adding database through flask-sqlalchemy orm         * added a simple database which holds project information         * added database configuration files         * added project_name model

--- server/server.py
+++ server/server.py
@@ -1,16 +1,29 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from database.database import db_session
+from database.database import init_db
+from models.project_name import project_name
 
 app = Flask("UraniumReborn")
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 
 @app.route("/", methods=['POST', 'GET'])
 def mainpage():
     if request.method == 'POST':
-        return request.form['Name']
+        pn = project_name(request.form['Name'])
+        db_session.add(pn)
+        db_session.commit()
+        result = project_name.query.all()
+        return str(result).replace("<", "").replace(">", "")
     else:
         return render_template("mainpage.html")
 
+init_db()
 app.run()
 
