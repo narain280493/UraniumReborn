ea8c6607fd1db1a65dc0db1ad71dbf59a72edbd6
narainsharma <narain280493@gmail.com>
2016-10-07 19:25:39 -0600

    debugging

--- server/server.py
+++ server/server.py
@@ -56,8 +56,8 @@ def mainpage():
 		p_title = request.form['apprenticeshipTitle']
 
 		print p_title
-		#p_website = url = urlparse.urlparse(request.form['apprenticeshipWebLink'])
-		#print p_website
+		p_website = urlparse.urlparse(request.form['apprenticeshipWebLink'])
+		print p_website
 		p_req = request.form['specialRequirement1'] +'::'+ request.form['specialRequirement2'] +'::'+ request.form['specialRequirement3'] + '::'+ request.form['specialRequirement4'] + '::'+ request.form['specialRequirement5'] 
 		print p_req
 		p_desc = request.form['apprenticeshipDescription']
