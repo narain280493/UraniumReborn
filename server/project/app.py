399b4ab6b82385290fc84fe83b0e63fac5f4d90f
Ranga <ranga91091@gmail.com>
2016-10-18 01:33:09 -0600

    created templates

--- server/project/app.py
+++ server/project/app.py
@@ -5,12 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
+@app.route('/student')
+def student():
+    return render_template('student.html')
+
+@app.route('/faculty')
+def faculty():
+    return render_template('faculty.html')
+
+@app.route('/listofprojects')
+def listofprojects():
+    return render_template('listofprojects.html')
 
 
 if __name__ == '__main__':
