7c85af2e58fcd3ae8864f48aba55c43c16a180bf
Athithyaa <athithyapanchapakesan@gmail.com>
2016-10-18 22:07:57 -0600

    Adding database support to the new server

--- server/project/app.py
+++ server/project/app.py
@@ -1,27 +1,41 @@
 from flask import Flask, render_template
+from database.database import init_db
+from database.database import db_session
+import os
 
-app = Flask(__name__)
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask("UraniumReborn", template_folder=tmpl_dir)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
+
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 @app.route('/student')
 def student():
     return render_template('student.html')
 
+
 @app.route('/faculty')
 def faculty():
     return render_template('faculty.html')
+
 
 @app.route('/listofprojects')
 def listofprojects():
@@ -29,4 +43,6 @@ def listofprojects():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    init_db()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
