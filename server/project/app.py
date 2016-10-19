f5c662831e293c5555146bcf51c332bf3f5aa689
Ram Das Diwakaran <ram.diwakaran@colorado.edu>
2016-10-18 20:55:21 -0600

    creating signup/login pages

--- server/project/app.py
+++ server/project/app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/student')
 def student():
