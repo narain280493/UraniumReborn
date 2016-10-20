0df17ae7e6e34eaf3dfec0c9d2809f028a366115
Athithyaa <athithyapanchapakesan@gmail.com>
2016-10-19 19:17:31 -0600

    Adding simple table using reactjs to the project list page

---
+++ server/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template
+from database.database import init_db
+from database.database import db_session
+import os
+from models.faculty import faculty
+from models.project import project
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask("UraniumReborn", template_folder=tmpl_dir)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/student')
+def student():
+    return render_template('student.html')
+
+
+@app.route('/faculty')
+def faculty_page():
+    return render_template('faculty.html')
+
+
+@app.route('/listofprojects')
+def listofprojects():
+    facs = faculty.query.all()
+    rows = []
+
+    for f in facs:
+        row = {"Faculty Name": f.faculty_name, "id": f.id, "Project Name": f.projects.title}
+        rows.append(row)
+
+    return render_template('listofprojects.html', pRows=rows)
+
+
+if __name__ == '__main__':
+    init_db()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
