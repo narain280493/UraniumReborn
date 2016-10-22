51cfa111e73ae4fb546764fd99b16c1cb3df2229
Athithyaa <athithyapanchapakesan@gmail.com>
2016-10-22 00:05:41 -0600

    HotFix: updating urlparse to match heroku python version

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
-from urllib.parse import urlparse
+from urlparse import urlparse
 
 
 def get_username(file):
