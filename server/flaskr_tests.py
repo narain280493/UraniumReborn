b314496be4a25da0de5b75eb6b267e26f1db39d2
Athithyaa <athithyapanchapakesan@gmail.com>
2016-10-21 23:50:33 -0600

    rectifying travis and updating requirements

--- server/flaskr_tests.py
+++ server/flaskr_tests.py
@@ -1,25 +1,9 @@
-import os
-from app import app as flaskApp
-from app import init_db
-import unittest
-import tempfile
+from flask import Flask
+from flask_testing import TestCase
 
 
-class AppTestCase(unittest.TestCase):
-    def setUp(self):
-        self.db_fd, flaskApp.config['DATABASE'] = tempfile.mkstemp()
-        flaskApp.config['TESTING'] = True
-        self.app = flaskApp.test_client()
-        with flaskApp.app_context():
-            init_db()
-
-    def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(flaskApp.config['DATABASE'])
-
-    def test_empty_db(self):
-        rv = self.app.get('/')
-        assert b'alongside graduate students' in rv.data
-
-if __name__ == "__main__":
-    unittest.main()
+class ServerTests(TestCase):
+    def create_app(self):
+        tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+        app = Flask('UraniumReborn', template_folder=tmpl_dir)
+        app.config['TESTING'] = True
